af3f10ed6d3fd133384736ee898b98baeff44d21
Malesela CHOCHO <mchocho@c5r5s10.wethinkcode.co.za>
2019-12-04 15:27:59 +0200

    HOWZIT Roger

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ app.get('/profile', (req, res) => {
 
 
 
-app.listen(3306, () => {
+app.listen(3000, () => {
 	console.log('Server started on port 3000');
 });
 
--- tests/email_test.js
+++ tests/email_test.js
@@ -7,22 +7,23 @@ async function main() {
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    // host: "smtp.ethereal.email",
+    service: 'gmail',
+    // port: 587,
+    //secure: false, // true for 465, false for other ports
     auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass // generated ethereal password
+        user: 'noreplycupidsarrow@gmail.com',
+        pass: 'cupidsarrowstaff'
     }
   });
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: 'js-hond@example.com', // sender address
-    to: "bar@example.com, gasal34043@mailt.top", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>" // html body
+    from: '"eldorado" <eldora36@ethereal.email>', // sender address
+    to: "xaxa@mailcatch.com", // list of receivers
+    subject: "HOWZIT", // Subject line
+    text: "Howzit my bru!!!!!", // plain text body
+    html: "<b>Aweh?</b>" // html body
   });
 
   console.log("Message sent: %s", info.messageId);
