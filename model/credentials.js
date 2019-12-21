cd3d05b864a568b8cc836953a29b88dd75d0e32f
sal'e chocho <malachocho01@gmail.com>
2019-12-22 01:45:44 +0200

    Can finally query DB

--- model/credentials.js
+++ model/credentials.js
@@ -1,5 +1,5 @@
 users = {
-	thanos: {
+	"thanos": {
 		host		: 'localhost',
 		user 		: 'root',
 		port		: '8080',
@@ -7,13 +7,19 @@ users = {
 		database  	: 'matcha',
 		socketPath	: '/goinfre/mchocho/Documents/mamp/mysql/tmp/mysql.sock'
 	  },
-	  roger: {
+	  "roger": {
 		host		: 'localhost',
 		user 		: 'root',
 		port		: '3000',
 		password	: 'pw123456',
 		database  	: 'matcha',
 		socketPath	: '/goinfre/rhobbs/Desktop/Server/mysql/tmp/mysql.sock'
+	  },
+	  "Titan": {
+		host		: 'localhost',
+		user 		: 'root',
+		password	: '654321',
+		database  	: 'matcha'
 	  }
 };
 
