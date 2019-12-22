ad72eb47aa21bd9ded13f1fca4980a6867c82597
sal'e chocho <malachocho01@gmail.com>
2019-12-22 23:05:03 +0200

    Included global 2 new global functions: validateUser and removeBlockedUser

--- includes/ft_util.js
+++ includes/ft_util.js
@@ -29,6 +29,10 @@ function ft_isobject(value) {
 	return (Object.prototype.toString.call(value) === '[object Object]');
 }
 
+function ft_isarray(value) {
+	return (Object.prototype.toString.call(value) === '[object Array]');
+}
+
 function ft_isEmptyObj(value) {
     for(var key in value) {
         if(value.hasOwnProperty(key))
@@ -55,6 +59,31 @@ function init_errorlist() {
 	};
 }
 
+function validateUser(res, sess) {
+	if (!ft_isobject(res))
+		return;
+	else if (!ft_isobject(sess))
+		res.redirect('/..');
+	else if (sess.verified !== 'T')
+		res.redirect('/verify_email');
+    else if (sess.valid !== 'T')
+        res.redirect('/reported_account');
+}
+
+function ft_removeBlockedUsers(matches, blacklist) {
+	return new Promise((resolve, reject) => {
+		blocked_users.forEach(function(value, index, arr) {
+			for (let i = 0, n = matches.length; i < n; i++) {
+				if (value.blocked_user === matches[i].id) {
+					matches = matches.splice(i, 1);
+					break;
+				}
+			}
+		});
+		resolve(matches);
+	});
+}
+
 module.exports.isstring = ft_isstring;
 module.exports.isnumber = ft_isnumber;
 module.exports.isfunction = ft_isfunction;
@@ -63,4 +92,8 @@ module.exports.isemail = ft_isemail;
 module.exports.init_errors = init_errorlist;
 module.exports.emptyObj = ft_isEmptyObj;
 module.exports.isobject = ft_isobject;
+module.exports.isarray = ft_isarray;
 module.exports.ranint = ft_ranint;
+module.exports.validateUser = validateUser;
+module.exports.removeBlockedUsers = ft_removeBlockedUsers;
+
