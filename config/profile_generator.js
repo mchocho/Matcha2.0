67ee31c9b2f298666bd9256971a772f0bad56c02
sal'e chocho <malachocho01@gmail.com>
2019-12-22 02:07:48 +0200

    Sample input now escapes all strings

--- config/profile_generator.js
+++ config/profile_generator.js
@@ -1,6 +1,5 @@
 const faker 		= require('faker'),
 	  util 			= require('util'),
-	  strescape 	= require('js-string-escape'),
 	  dbc 			= require('../model/sql_connect.js'),
 	  ft_util 		= require('../includes/ft_util.js');
 
@@ -8,21 +7,21 @@ function generate_user(i) {
 	if (i === 40)
 		return;
 	const user = [
-		strescape(faker.name.findName()),	//0
-		strescape(faker.name.firstName()),	//1
-		strescape(faker.name.lastName()),	//2
-		strescape(['M', 'F'][ft_util.ranint(2)]),	//3
-		strescape(['M', 'F', 'B'][ft_util.ranint(2)]),	//4
-		strescape(faker.date.past()),	//5
-		strescape(faker.internet.email()),	//6
-		'54321',	//7
-		['T', 'F'][ft_util.ranint(1)],	//8
+		faker.name.findName(),				//0
+		faker.name.firstName(),				//1
+		faker.name.lastName(),				//2
+		['M', 'F'][ft_util.ranint(2)],		//3
+		['M', 'F', 'B'][ft_util.ranint(2)],	//4
+		faker.date.past(),					//5
+		faker.internet.email(),				//6
+		'OMG42',							//7
+		['T', 'F'][ft_util.ranint(1)],		//8
 		'T',
 		'T',
 		''		
 	];
 		
-	let sql = `SELECT id FROM users WHERE username = '${user[0]}' OR email = '${user[6]}'`;
+	let sql = `SELECT id FROM users WHERE username = ? OR email = ?`;
 	dbc.query(sql, [[user[0], user[6]]], function(err, result) {
 		if (err) throw err;
 		if (result.length > 0)
@@ -36,7 +35,13 @@ function generate_user(i) {
 			if (err) throw err;
 			console.log("Number of records inserted: " + result.affectedRows);
 			sql = "INSERT INTO images (name, user_id, profile_pic) VALUES (?)";
-			dbc.query(sql, [[faker.random.image(), result.insertId, 'T']], function(err, result) {
+			dbc.query(sql, [
+					[faker.random.image(), result.insertId, 'T'],
+					[faker.random.image(), result.insertId, 'F'],
+					[faker.random.image(), result.insertId, 'F'],
+					[faker.random.image(), result.insertId, 'F'],
+					[faker.random.image(), result.insertId, 'F']
+				], function(err, result) {
 				if (err) throw err;
 				generate_user(i++);
 			});
