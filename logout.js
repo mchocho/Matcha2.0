22166d7058ab36efd545a87d7dae47f22290374b
sal'e chocho <malachocho01@gmail.com>
2019-12-29 00:52:01 +0200

    Logout route

--- logout.js
+++ logout.js
@@ -1,23 +1,12 @@
-const express 		= require('express'),
-	  path			= require('path'),
-	  mysql			= require('mysql'),
-	  URL			= require('url'),
+const 	  express 		= require('express'),
 	  session 		= require('express-session'),
-	  uuidv4 		= require('uuid/v4'),
-	  ft_util		= require('./includes/ft_util.js'),
-	  app 			= express(),
-	  nodemailer 	= require('nodemailer'),
-	  os			= require('os');
-
+	  app 			= express();
 
 let router = express.Router();
 module.exports = router;
-app.use(session({secret: uuidv4(), cookie: {}, resave: false, saveUninitialized: true}));
-
 
 router.get('/', (req, res) => {
-    
     req.session.destroy(function() {
-        res.redirect('/signin');
+        res.redirect('/');
     });
 });
