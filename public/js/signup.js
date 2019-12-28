43ff1089a8826e3402101db3815e4003c5b92ae2
sal'e chocho <malachocho01@gmail.com>
2019-12-29 00:46:11 +0200

    Some page styling and animations

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,11 +1,27 @@
 function script() {
 	const dob = document.getElementById('dob'),
 		gender_btns = document.getElementById('gender_group').childNodes,
-		preference_btns = document.getElementById('preference_group').childNodes;
+		preference_btns = document.getElementById('preference_group').childNodes,
+		storage = storageAvailable('session'),
+		formIds = ['username', 'f_name', 'l_name', 'gender', 'preference', 'dob', 'email'];
 	flatpickr(dob, {});
 	function isNode(el) {
 		return (el instanceof Element);
 	}
+	function storageAvailable(type) {
+	    var storage;
+	    try {
+	        storage = window[type];
+	        var x = 'March_21_95';
+	        storage.setItem(x, x);
+	        storage.removeItem(x);
+	        return true;
+	    }
+	    catch(e) {
+	        return e instanceof DOMException && (e.code === 22 || e.code === 1014 || e.name === 'QuotaExceededError' || e.name === 'NS_ERROR_DOM_QUOTA_REACHED') && (storage && storage.length !== 0);
+	    }
+	}
+
 	for (let i = 0, n = gender_btns.length; i < n; i++) {
 		gender_btns[i].addEventListener('click', function(e) {
 			document.getElementById('gender').setAttribute('value', e.target.textContent);
@@ -26,5 +42,32 @@ function script() {
 			e.target.classList.add('option');
 		});
 	}
+
+	document.getElementById('signup').addEventListener('change', function() {
+		formIds.forEach(function(value) {
+			if (storageAvailable)
+				sessionStorage.setItem(value, document.getElementById(value).value);
+		});
+	});
+
+	formIds.forEach(function(value) {
+		const item = sessionStorage.getItem(value).trim();
+		if (item.length > 0) {
+			if (value === 'gender') {
+				if (item === 'Female')
+					gender_btns[0].classList.add('option');
+				else
+					gender_btns[1].classList.add('option');
+			} else if (value === 'preference') {
+				if (item === 'Female')
+					preference_btns[0].classList.add('option');
+				else if (item === 'Male')
+					preference_btns[1].classList.add('option');
+				else
+					preference_btns[2].classList.add('option');
+			}
+			document.getElementById(value).value = sessionStorage.getItem(value);
+		}
+	});
 }
 document.addEventListener("DOMContentLoaded", script);
