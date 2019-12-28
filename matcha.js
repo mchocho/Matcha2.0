f319b83f9dc2516b25144d97c968328963a9d679
sal'e chocho <malachocho01@gmail.com>
2019-12-29 00:53:33 +0200

    Users can now view their list of possible companions

--- matcha.js
+++ matcha.js
@@ -2,10 +2,10 @@ const express 		= require('express'),
 	  session	    = require('express-session'),
 	  path			= require('path'),
 	  mysql			= require('mysql'),
-	  googleMapsClient = require('@google/maps').createClient({key: 'AIzaSyAZBn1NrjeC0gbFW4Fua4XEHudaTwvpy2Q'}),
 	  body_p		= require('body-parser'),
 	  URL			= require('url'),
 	  util 			= require('util'),
+	  geo			= require('geolocation-utils'),
 	  ft_util		= require('./includes/ft_util.js'),
 	  dbc			= require('./model/sql_connect.js'),
 	  email			= require('./includes/mail_client.js');
@@ -20,110 +20,71 @@ router.get('/', (req, res) => {
 		location,
 		matches;
 
-	if (!ft_util.isobject(sess))
+	if (!ft_util.isobject(sess)) {
 		res.redirect('/..');
-	else if (sess.verified !== 'T')
+		return;
+	}
+	else if (sess.verified !== 'T') {
 		res.redirect('/verify_email');
-	else if (sess.valid !== 'T')
+		return;
+	}
+	else if (sess.valid !== 'T') {
 		res.redirect('/reported_account');
+		return;
+	}
+
 	dbc.query(sql, [sess.id], (err, result) => {
-		if (err)
-			throw err;
-		if (result.length === 0)
-			res.redirect('/profile');
+		if (err) throw err;
+		if (result.length === 0) {
+			res.redirect('/user');
+			console.log('User has no location data!');
+			return
+		}
+
 		location = result[0];
 		sql = "SELECT blocked_user FROM blocked_accounts WHERE user_id = ?";
 		dbc.query(sql, [sess.id], (err, result) => {
 			blacklist = result;
-			if (sess.preferences === 'B')
-			{
-				sql = "SELECT * FROM users WHERE gender = 'F' AND (preferences = ? OR preferences = 'B') AND verified = 'T' AND NOT id = ?";
-				dbc.query(sql, [sess.gender, sess.id], (err, result) => {
-					if (err) throw err;
-					matches = result;
-					sql = "SELECT * FROM users WHERE gender = 'M' AND (preferences = ? OR preferences = 'B') AND verified = 'T' AND NOT id = ?";
-					dbc.query(sql, [sess.gender, sess.id], (err, result) => {
-						if (err) throw err;
-						ft_util.removeBlockedUsers(matches.concat(result), blacklist)
-						.then(values => {
-							for (let i = 0, n = values.length; i < n; i++) {
-								sql ="SELECT name FROM images WHERE user_id = ? AND profile_pic = 'T'";
-								dbc.query(sql, [values[i].id], (err, result) => {
-									if (err) throw err;
-									values[i].image = result[0];
-									sql = "SELECT * FROM locations WHERE user_id = ?";
-									dbc.query(sql, [values[i].id], (err, result) => {
-										if (err) throw err;
-										values[i].location = result[0];
-										googleMapsClient.distanceMatrix({
-											origins: [{lat: location.location[0], location.location[1]}],
-											destinations: [{lat: result[0].location[0], lng: result[0].location[1]}],
-											mode: 'walking',
-											units: 'metric'
-										}, (err, response) => {
-											if (!err) {
-												values[i].location = response.rows[0].elements[0];
-										    } else if (err === 'timeout') {
-										      // Handle timeout.
-										    } else if (err.json) {
-										      // Inspect err.status for more info.
-										    } else {
-										      // Handle network error.
-										    }
-										});
-									});
-								});
-							}
-							res.render('matcha.pug', {
-								title: "Find your match | Cupid's Arrow",
-								users: values
-							});
-						}).catch(err => {
-							throw err;
-						});
-					});
-				});
-			}
+			if (sess.preferences === 'M')
+				sql = "SELECT * FROM users WHERE gender = 'M' AND (preferences = ? OR preferences = 'B') AND verified = 'T' AND NOT id = ?";
 			else if (sess.preferences === 'F')
 				sql = "SELECT * FROM users WHERE gender = 'F' AND (preferences = ? OR preferences = 'B') AND verified = 'T' AND NOT id = ?";
 			else
-				sql = "SELECT * FROM users WHERE gender = 'M' AND (preferences = ? OR preferences = 'B') AND verified = 'T' AND NOT id = ?";
+				sql = "SELECT * FROM users WHERE (preferences = ? OR preferences = 'B') AND verified = 'T' AND NOT id = ?";
 			dbc.query(sql, [sess.gender, sess.id], (err, result) => {
 				if (err) throw err;
-				ft_util.removeBlockedUsers(matches.concat(result), blacklist)
+				ft_util.removeBlockedUsers(result, blacklist)
 				.then(values => {
-					for (let i = 0, n = values.length; i < n; i++) {
-						sql ="SELECT name FROM images WHERE user_id = ? AND profile_pic = 'T'";
-						dbc.query(sql, [values[i].id], (err, result) => {
-							if (err) throw err;
-							values[i].image = result[0];
-							sql = "SELECT * FROM locations WHERE user_id = ?";
+					if (values.length > 0)
+						for (let i = 0, n = values.length; i < n; i++) {
+							sql ="SELECT name FROM images WHERE user_id = ? AND profile_pic = 'T'";
+							values[i]['url'] = '/profile/' + values[i].id;
 							dbc.query(sql, [values[i].id], (err, result) => {
 								if (err) throw err;
-								values[i].location = result[0];
-								googleMapsClient.distanceMatrix({
-									origins: [{lat: location[0], location[1]}],
-									destinations: [{lat: result[0][0], lng: result[0][1]}],
-									mode: 'walking',
-									units: 'metric'
-								}, (err, response) => {
-									if (!err) {
-										values[i].location = response.rows[0].elements[0];
-								    } else if (err === 'timeout') {
-								      // Handle timeout.
-								    } else if (err.json) {
-								      // Inspect err.status for more info.
-								    } else {
-								      // Handle network error.
-								    }
+								values[i].images = result;
+								sql = "SELECT * FROM locations WHERE user_id = ?";
+								dbc.query(sql, [values[i].id], (err, result) => {
+									if (err) throw err;
+									if (result.length === 0) {
+										values.splice(i, 1);
+										i--;
+										return;
+									}
+									values[i]['distance'] = geo.distanceTo({lat: location.lat, lon: location.lng}, {lat: result[0]['lat'], lon: result[0]['lng']}).toFixed(2);
+									//💩💩💩
+									if (i === values.length - 1)
+										res.render('matcha.pug', {
+											title: "Find your match | Cupid's Arrow",
+											users: values
+										}); 
 								});
 							});
-						});
-					}
-					res.render('matcha.pug', {
-						title: "Find your match | Cupid's Arrow",
-						users: values
-					});
+						}
+					else
+						res.render('matcha.pug', {
+							title: "Find your match | Cupid's Arrow",
+							users: []
+						}); 
 				}).catch(err => {
 					throw err;
 				});
