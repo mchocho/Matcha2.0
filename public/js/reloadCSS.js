cab0dac1982b26e1c04842e15551d1715cba75d6
sal'e chocho <malachocho01@gmail.com>
2020-01-10 00:06:33 +0200

    I love this script

--- public/js/reloadCSS.js
+++ public/js/reloadCSS.js
@@ -1,19 +1,18 @@
 function script() {
 	function reloadCSS() {
 		const links = document.getElementsByTagName('link');
-                         // y_coords = window.pageYoffset;
-                         // y_coords = window.pageYoffset;
+		console.log('Refreshing CSS');
 		for (let i = 0, n = links.length; i < n; i++) {
+			if (links[i].href.indexOf('bootstrap') > -1)
+				continue;
 			links[i].href += "?";
                 }
-                // window.scrollTo(0, y_coords);
 		console.log('CSS page refreshed.');
                 return;
         };
 	
 	window.addEventListener('keyup', function(e) {
 		if (e.keyCode === 101 || e.keyCode === 53) {    //Numpad 5
-			console.log('Running reloadCSS');
 			reloadCSS();
 		}
 		return;
