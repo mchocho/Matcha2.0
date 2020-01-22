893bc2aefa5c982ce8e320b9e40f592875a7c111
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2020-01-22 13:04:37 +0200

    Removed unused requires

--- signup.js
+++ signup.js
@@ -1,12 +1,5 @@
 const 	  express 		= require('express'),
-      	  session	    	= require('express-session'),
-	  path			= require('path'),
-	  mysql			= require('mysql'),
-	  body_p		= require('body-parser'),
 	  moment		= require('moment'),
-	  nodemailer 		= require('nodemailer'),
-	  os			= require('os'),
-	  util			= require('util'),
 	  uuidv4 		= require('uuid/v4'),
 	  bcrypt		= require('bcrypt');
 	  ft_util		= require('./includes/ft_util.js'),
