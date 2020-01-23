51ef8a3e4eb42dcf00eed09f830ba78e12c0f353
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-01-23 14:00:19 +0200

    Added tests for hasuppercase ans haslowercase

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -1,5 +1,7 @@
 const ft_util = require('../includes/ft_util');
 
-let bool = ft_util.hasNumber('           ');
-
+let string = 'qw12QW!@';
+let bool = ft_util.hasuppercase(string);
 console.log(bool);
+bool = ft_util.haslowercase(string);
+console.log(bool); 
