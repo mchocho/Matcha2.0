3dc8360922a0eba77ec66df8b3aac922f2e3d75a
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-01-24 11:48:05 +0200

    Busy testing escaping html tags

---
+++ tests/test_strip_tags.js
@@ -0,0 +1,21 @@
+let text = "Mc\"Don<ald";
+cleanText = text.replace(/<\/?[^>]+(>|$)/g, "");
+
+var tagsToReplace = {
+    '&': '&amp;',
+    '<': '&lt;',
+	'>': '&gt;',
+	"'": '&#39;',
+	'"': '&#34;'
+};
+
+function replaceTag(tag) {
+    return tagsToReplace[tag] || tag;
+}
+
+function safe_tags_replace(str) {
+    return str.replace(/[&<>]/g, replaceTag);
+}
+
+text = safe_tags_replace(text);
+console.log(text);
