754d0e21f9991b7303f72d966bb9b86b47dee1c0
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-01-28 17:30:49 +0200

    More moding, code is still the same just modded out

--- matcha.js
+++ matcha.js
@@ -41,61 +41,61 @@ router.get('/', (req, res) => {
 			return;
 		}
 		location = result[0];
-		dbc.query(sql.selBlockedUsers, [sess.id], findPrefrences)
+		dbc.query(sql.selBlockedUsers, [sess.id], findPrefrences);
+	}
 		
-		function findPrefrences(err, result) {
-			if (err) {throw err}
-			blacklist = result;
-			if (sess.preferences === 'M') {
-				sql = "SELECT * FROM users WHERE gender = 'M' AND (preferences = ? OR preferences = 'B') AND verified = 'T' AND NOT id = ?";
-			} else if (sess.preferences === 'F') {
-				sql = "SELECT * FROM users WHERE gender = 'F' AND (preferences = ? OR preferences = 'B') AND verified = 'T' AND NOT id = ?";
-			} else {
-				sql = "SELECT * FROM users WHERE (preferences = ? OR preferences = 'B') AND verified = 'T' AND NOT id = ?";
-			}
-			dbc.query(sql, [sess.gender, sess.id], (err, result) => {
-				if (err) throw err;
-				ft_util.removeBlockedUsers(result, blacklist)
-				.then(values => {
-					if (values.length > 0) {
-						for (let i = 0, n = values.length; i < n; i++) {
-							sql ="SELECT name FROM images WHERE user_id = ? AND profile_pic = 'T'";
-							values[i]['url'] = '/profile/' + values[i].id;
+	function findPrefrences(err, result) {
+		if (err) {throw err}
+		blacklist = result;
+		if (sess.preferences === 'M') {
+			sql = "SELECT * FROM users WHERE gender = 'M' AND (preferences = ? OR preferences = 'B') AND verified = 'T' AND NOT id = ?";
+		} else if (sess.preferences === 'F') {
+			sql = "SELECT * FROM users WHERE gender = 'F' AND (preferences = ? OR preferences = 'B') AND verified = 'T' AND NOT id = ?";
+		} else {
+			sql = "SELECT * FROM users WHERE (preferences = ? OR preferences = 'B') AND verified = 'T' AND NOT id = ?";
+		}
+		dbc.query(sql, [sess.gender, sess.id], (err, result) => {
+			if (err) throw err;
+			ft_util.removeBlockedUsers(result, blacklist)
+			.then(values => {
+				if (values.length > 0) {
+					for (let i = 0, n = values.length; i < n; i++) {
+						sql ="SELECT name FROM images WHERE user_id = ? AND profile_pic = 'T'";
+						values[i]['url'] = '/profile/' + values[i].id;
+						dbc.query(sql, [values[i].id], (err, result) => {
+							if (err) throw err;
+							values[i].images = result;
+							sql = "SELECT * FROM locations WHERE user_id = ?";
 							dbc.query(sql, [values[i].id], (err, result) => {
 								if (err) throw err;
-								values[i].images = result;
-								sql = "SELECT * FROM locations WHERE user_id = ?";
-								dbc.query(sql, [values[i].id], (err, result) => {
-									if (err) throw err;
-									if (result.length === 0) {
-										console.log("VID", values[i].id);
-										console.log("XXXXXX");
-										values.splice(i, 1);
-										i--;
-										return;
-									}
-									values[i]['distance'] = geo.distanceTo({lat: location.lat, lon: location.lng}, {lat: result[0]['lat'], lon: result[0]['lng']}).toFixed(2);
-									console.log("Here", i, n - 1);
-									if (i === n - 1) {
-										console.log("Hello render");
-										res.render('matcha.pug', {
-											title: "Find your match | Cupid's Arrow",
-											users: values
-										});
-									}
-								});
+								if (result.length === 0) {
+									console.log("VID", values[i].id);
+									console.log("XXXXXX");
+									values.splice(i, 1);
+									i--;
+									return;
+								}
+								values[i]['distance'] = geo.distanceTo({lat: location.lat, lon: location.lng}, {lat: result[0]['lat'], lon: result[0]['lng']}).toFixed(2);
+								console.log("Here", i, n - 1);
+								if (i === n - 1) {
+									console.log("Hello render");
+									res.render('matcha.pug', {
+										title: "Find your match | Cupid's Arrow",
+										users: values
+									});
+								}
 							});
-						}
-					} else {
-						res.render('matcha.pug', {
-							title: "Find your match | Cupid's Arrow",
-							users: []
-						}); 
+						});
 					}
-				}).catch(err => {
-					throw err;
-				});
+				} else {
+					res.render('matcha.pug', {
+						title: "Find your match | Cupid's Arrow",
+						users: []
+					}); 
+				}
+			}).catch(err => {
+				throw err;
 			});
-		};
+		});
 	};
 });
