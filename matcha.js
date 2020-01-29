33b616b6248bd7b0e8c74f0b955c747f3b42462c
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2020-01-29 08:23:10 +0200

    Cleaning up the moding, seperated findPref

--- matcha.js
+++ matcha.js
@@ -54,48 +54,50 @@ router.get('/', (req, res) => {
 		} else {
 			prefSql = sql.selAllBoth;
 		}
-		dbc.query(prefSql, [sess.gender, sess.id], (err, result) => {
-			if (err) throw err;
-			ft_util.removeBlockedUsers(result, blacklist)
-			.then(values => {
-				if (values.length > 0) {
-					for (let i = 0, n = values.length; i < n; i++) {
-						let ssql ="SELECT name FROM images WHERE user_id = ? AND profile_pic = 'T'";
-						values[i]['url'] = '/profile/' + values[i].id;
-						dbc.query(ssql, [values[i].id], (err, result) => {
-							if (err) throw err;
-							values[i].images = result;
-							ssql = "SELECT * FROM locations WHERE user_id = ?";
-							dbc.query(ssql, [values[i].id], (err, result) => {
-								if (err) throw err;
-								if (result.length === 0) {
-									console.log("VID", values[i].id);
-									console.log("XXXXXX");
-									values.splice(i, 1);
-									i--;
-									return;
-								}
-								values[i]['distance'] = geo.distanceTo({lat: location.lat, lon: location.lng}, {lat: result[0]['lat'], lon: result[0]['lng']}).toFixed(2);
-								console.log("Here", i, n - 1);
-								if (i === n - 1) {
-									console.log("Hello render");
-									res.render('matcha.pug', {
-										title: "Find your match | Cupid's Arrow",
-										users: values
-									});
-								}
+		dbc.query(prefSql, [sess.gender, sess.id], getMatches);
+	}
+
+	function getMatches(err, result) {
+	if (err) throw err;
+	ft_util.removeBlockedUsers(result, blacklist)
+	.then(values => {
+		if (values.length > 0) {
+			for (let i = 0, n = values.length; i < n; i++) {
+				let ssql ="SELECT name FROM images WHERE user_id = ? AND profile_pic = 'T'";
+				values[i]['url'] = '/profile/' + values[i].id;
+				dbc.query(ssql, [values[i].id], (err, result) => {
+					if (err) throw err;
+					values[i].images = result;
+					ssql = "SELECT * FROM locations WHERE user_id = ?";
+					dbc.query(ssql, [values[i].id], (err, result) => {
+						if (err) throw err;
+						if (result.length === 0) {
+							console.log("VID", values[i].id);
+							console.log("XXXXXX");
+							values.splice(i, 1);
+							i--;
+							return;
+						}
+						values[i]['distance'] = geo.distanceTo({lat: location.lat, lon: location.lng}, {lat: result[0]['lat'], lon: result[0]['lng']}).toFixed(2);
+						console.log("Here", i, n - 1);
+						if (i === n - 1) {
+							console.log("Hello render");
+							res.render('matcha.pug', {
+								title: "Find your match | Cupid's Arrow",
+								users: values
 							});
-						});
-					}
-				} else {
-					res.render('matcha.pug', {
-						title: "Find your match | Cupid's Arrow",
-						users: []
-					}); 
-				}
-			}).catch(err => {
-				throw err;
-			});
+						}
+					});
+				});
+			}
+		} else {
+			res.render('matcha.pug', {
+				title: "Find your match | Cupid's Arrow",
+				users: []
+			}); 
+		}
+		}).catch(err => {
+			throw err;
 		});
 	};
 });
