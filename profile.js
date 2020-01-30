2b380040ad3f61f5e50538c45ca88afa75fd5218
sal'e chocho <malachocho01@gmail.com>
2020-01-30 10:33:37 +0200

    Profile route

--- profile.js
+++ profile.js
@@ -3,12 +3,17 @@ const express 		= require('express'),
 	  path			= require('path'),
 	  mysql			= require('mysql'),
 	  body_p		= require('body-parser'),
+	  uuidv4 		= require('uuid/v4'),
+	  nodemailer	= require('nodemailer'),
 	  geo			= require('geolocation-utils'),
 	  moment		= require('moment'),
 	  os			= require('os'),
 	  util			= require('util'),
 	  ft_util		= require('./includes/ft_util.js'),
-	  dbc			= require('./model/sql_connect.js');
+	  dbc			= require('./model/sql_connect.js'),
+	  sql			= require('./model/sql_statements.js'),
+	  email			= require('./includes/mail_client.js'),
+	  msgTemplates 	= require('./includes/email_templates.js');
 
 let router = express.Router();
 module.exports = router;
@@ -78,14 +83,14 @@ router.get('/:id?', (req, res) => {
 									dbc.query(sql, [[id, result.insertId, 'views']], (err, result) => {
 										if (err) throw err;
 										user['age'] = moment(user.DOB).fromNow(true);
-										//TEST
-										console.log('Profile object --> ' + util.inspect({
-											title: user.username,
-											notifications: req.session.notifications,
-											chats: req.session.chats,
-											user: user
-										}));
-										//ENDOF TEST
+										if (ft_util.VERBOSE) {
+											console.log('Profile object --> ' + util.inspect({
+												title: user.username,
+												notifications: req.session.notifications,
+												chats: req.session.chats,
+												user: user
+											}));
+										}
 										res.render('profile.pug', {
 											title: user.username,
 											notifications: req.session.notifications,
@@ -101,5 +106,81 @@ router.get('/:id?', (req, res) => {
 				});
 			});
 		});
+	});
+}).post('/connect:profile?', (req, res) => {
+	const sess = req.session.user,
+	      profile = req.params.profile.replace(/\./g, '');
+	let sql = "SELECT * FROM likes WHERE ((liker = ? AND liked = ?) OR (liker = ? AND liked = ?))",
+	    userLikesYou = false,
+	    youLikeUser = false,
+	    json = `{"service": "connect", "profile": "${profile}", `;
+
+	res.writeHead(200, {"Content-Type": "text/plain"});	//Allows us to respond to the client
+	if (!ft_util.isobject(sess) || sess.verified !== 'T' || sess.valid !== 'T' || isNaN(profile)) {
+		res.end(json + '"result": "Failed"}');
+        return;
+	}
+		
+	if (ft_util.VERBOSE) {
+		console.log('STATUS: ' + res.statusCode);
+  		console.log('HEADERS: ' + JSON.stringify(res.headers));
+	}
+
+	dbc.query(sql, [sess.id, profile, profile, sess.id, 'T'], (err, result) => {
+		if (err) {throw err}
+		for (let i = 0, n = result.length; i < n; i++) {
+			if (result[i].liker === Number(profile))
+				userLikesYou = true;
+			if (result[i].liker === Number(sess.id))
+				youLikeUser = true;
+		}
+		if (youLikeUser === true)
+			sql = "DELETE FROM likes WHERE liker = ? AND liked = ?";
+		else
+			sql = "INSERT INTO likes (liker, liked) VALUES (?)";
+
+		dbc.query(sql, (youLikeUser === true ) ? [sess.id, Number(profile)] : [[sess.id, Number(profile)]], (err, result) => {
+			if (err) {throw err}
+			if (result.affectedRows === 1) {
+				sql = "INSERT INTO notifications (user_id, service_id, type) VALUES (?)";
+				dbc.query(sql, [[Number(profile), result.insertId, (youLikeUser === true) ? 'unlike' : 'like']], (err, result) => {
+					if (err) {throw err}
+					res.end(json + `"result": "Success", "youLikeUser": "${!youLikeUser}", "userLikesYou": "${userLikesYou}" }`);
+				});
+			} else {
+				res.end(json + '"result": "Failed"}');
+			}
+		});
+	});
+}).post('/report:profile?', (req, res) => {
+	const sess = req.session.user,
+	      profile = req.params.profile.replace(/\./g, ''),
+	      token = uuidv4(),
+	      url = "http://localhost:3000/verification/?key=" + token;
+	let sql = "UPDATE users SET valid = 'F' WHERE id = ?",
+	    json = `{"service": "report_profile", "profile": "${profile}", `;
+
+	res.writeHead(200, {"Content-Type": "text/plain"});	//Allows us to respond to the client
+	if (!ft_util.isobject(sess) || sess.verified !== 'T' || sess.valid !== 'T' || isNaN(profile)) {
+		res.end(json + '"result": "Failed"}');
+        return;
+	}
+	if (ft_util.VERBOSE) {
+		console.log('STATUS: ' + res.statusCode);
+  		console.log('HEADERS: ' + JSON.stringify(res.headers));
+	}
+
+	dbc.query(sql, [Number(profile)], (err, result) => {
+		if (err) throw err;
+		if (result.affectedRows === 1) {
+			sql = "SELECT email FROM users WHERE id = ?";
+			dbc.query(sql, [sess.id], (err, result) => {
+				if (err) throw err;
+				email.main(result.email, "Your profile has been reported | Cupid's Arrow", msgTemplates.report_account(url)).catch(console.error);
+				res.end(json + '"result": "Success"}');
+			});
+		} else {
+			res.end(json + '"result": "Failed"}');
+		}
 	});
 });
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,49 @@
 function script() {
     const   profile_id = document.getElementById('profile_id').value,
+    		profile_name = document.getElementById('username').textContent,
             connect_status = document.getElementById('connection_status'),
-            connection_btn = document.getElementById('connection_btn');
+            connection_btn = document.getElementById('connection_btn'),
+            block_btn = document.getElementById('blockuser_btn'),
+            reportuser_btn = document.getElementById('reportuser_btn');
           
     connection_btn.addEventListener('click', function(e) {
-        xhr('/admin/connect/' + profile_id, 'POST', null, function(xhr) {
-            //handle request
-
+        xhr('/profile/connect.' + parseInt(profile_id), 'POST', null, function(xhr) {
+        	const res = JSON.parse(xhr.responseText);
+			if (res.result === 'Success') {
+				if (res.youLikeUser === 'true') {
+					connection_btn.textContent = 'Disconnect';
+					if (res.userLikesYou === 'true') {
+						//Connected
+						connection_status.textContent = 'You and ' + profile_name + ' are both connected';
+					} else {
+						//User doesn't like you, sorry
+						connection_status.textContent = 'It seems like ' + profile_name + " hasn't liked you back, hang in there.";
+					}
+				} else {
+					connection_btn.textContent = 'Connect';
+					if (res.userLikesYou === 'true') {
+						connection_status.textContent = profile_name + ' now likes you, like them back to get to know more about them.';
+					} else {
+						connection_status.textContent = 'You and ' + profile_name + ' are not connected';
+					}
+				}
+			}
         });
     }, true);
+
+    block_btn.addEventListener('click', function(e) {
+    	xhr('/profile/block.' + parseInt(profile_id), 'POST', null, function(xhr) {
+    		const res = JSON.parse(xhr.responseText);
+    	});
+    });
+
+    reportuser_btn.addEventListener('click', function(e) {
+    	xhr('/profile/report.' + parseInt(profile_id), 'POST', null, function(xhr) {
+    		const res = JSON.parse(xhr.responseText);
+    	});
+    });
+
+
 }
 
 document.addEventListener("DOMContentLoaded", script);
