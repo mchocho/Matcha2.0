d2aa423c3a09e04bacf265e6ff1549d80e07ca3b
veronar <vramkuma@student.wethinkcode.co.za>
2021-01-10 20:42:37 +0200

    refactored chat to chat.js

---
+++ public/js/chat.js
@@ -0,0 +1,32 @@
+var socket = io();
+console.log('front side socket connected');
+
+var messages = document.getElementById('messages');
+var form = document.getElementById('form');
+var input = document.getElementById('m');
+var username = document.getElementById('username');
+
+socket.emit('add user', username.innerText);
+
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+	if (input.value) {
+		socket.emit('chat message', input.value, username.innerText);
+		input.value = '';
+	}
+});
+
+socket.on('chat message', (data) => {
+	var userDiv = document.createElement('span');
+	userDiv.setAttribute("class", "username");
+	userDiv.textContent = data.user;
+
+	var msgDiv = document.createElement('span');
+	msgDiv.setAttribute("class", "msgBody");
+	msgDiv.textContent = data.msg;
+
+	var item = document.createElement('li');
+	item.append(userDiv, msgDiv);
+	messages.appendChild(item);
+	window.scrollTo(0, document.body.scrollHeight);
+});
