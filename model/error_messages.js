324561a3a7df2a3950a838afcab8840dfd04cd97
e <mchocho@student.wethinkcode.co.za>
2021-01-13 20:15:27 +0000

    Some unused error messages

--- model/error_messages.js
+++ model/error_messages.js
@@ -1,4 +1,5 @@
 module.exports = {
-    invalidID:      "Tried to select profile using invalid data type for key id (NaN returned true)",
-    fameRatingErr:  "Failed to update user rating."
+    invalidID:        "Tried to select profile using invalid data type for key id (NaN returned true)",
+    fameRatingErr:    "Failed to update user rating.",
+    fileUploadFailed: "Failed to upload your file"
 }
