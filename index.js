5195dd235fadf406023bea7e1017b8d965c4ccc8
e <mchocho@student.wethinkcode.co.za>
2021-01-30 20:24:11 +0200

    Updating a few src/ files

--- index.js
+++ index.js
@@ -10,29 +10,34 @@ const flash       = require("connect-flash");
 const app         = express();
 const PORT        = 3000;
 
-var server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const server      = require("http").createServer(app);
+const io          = require("socket.io")(server);
 
 // CHAT
-io.on('connection', (socket) => {
-	console.log('a user connected - index.js');
+io.on("connection", (socket) =>
+{
+  console.log('a user connected - index.js');
 
-	socket.on('disconnect', () => {
-		console.log('user disconnected');
-	});
+  socket.on('disconnect', () =>
+  {
+    console.log('user disconnected');
+  });
 
-	socket.on('add user', (username) => {
-		socket.username = username;
-		console.log('Socket user: ' + socket.username);
-	});
+  socket.on('add user', (username) =>
+  {
+    socket.username = username;
+    console.log('Socket user:', socket.username);
+  });
 
-	socket.on('chat message', (msg) => {
-		console.log('message: ' + msg);
-		io.emit('chat message', {
-			user: socket.username,
-			msg
-		});
-	});
+  socket.on('chat message', (msg) =>
+  {
+    console.log('message:', msg);
+
+    io.emit('chat message', {
+      user: socket.username,
+      msg
+    });
+  });
 });
 
 app.use(flash());
@@ -66,8 +71,32 @@ app.use("/verification", verifyUserEmail);
 let forgotPassword = require("./src/forgot_password");
 app.use("/forgot_password", forgotPassword);
 
+let matchaRouter = require("./src/matcha");
+app.use("/matcha", matchaRouter);
+
 let userRouter = require("./src/user");
 app.use("/user", userRouter);
+
+let uploadRouter = require("./api/upload");
+app.use("/upload", uploadRouter);
+
+let fullnameRouter = require("./api/fullname");
+app.use("/fullname", fullnameRouter);
+
+let usernameRouter = require("./api/username");
+app.use("/username", usernameRouter);
+
+let emailRouter = require("./api/email");
+app.use("/email", emailRouter);
+
+let passwordRouter = require("./api/password");
+app.use("/password", passwordRouter);
+
+let dobRouter = require("./api/dob");
+app.use("/dob", dobRouter);
+
+let biographyRouter = require("./api/biography");
+app.use("/biography", biographyRouter);
 
 let genderRouter = require("./api/gender");
 app.use("/gender", genderRouter);
@@ -80,9 +109,6 @@ app.use("/preferences", preferencesRouter);
 
 let locationRouter = require("./api/location");
 app.use("/location", locationRouter);
-
-let matchaRouter = require("./src/matcha");
-app.use("/matcha", matchaRouter);
 
 let profileRouter = require("./src/profile");
 app.use("/profile", profileRouter);
@@ -99,11 +125,11 @@ app.use("/block", blockRouter);
 let notificationsRouter = require("./src/notifications");
 app.use("/notifications", notificationsRouter);
 
-let logoutRouter = require("./src/logout");
-app.use("/logout", logoutRouter);
-
 let chatRouter = require("./src/chat");
 app.use("/chat", chatRouter);
+
+let logoutRouter = require("./src/logout");
+// app.use("/logout", logoutRouter);
 
 let _404Router = require("./src/404");
 app.use("*", _404Router);
